92c5f8dbcb432041b6ddf8961f5400569add58b1
Chillen <josh.jenkin@live.com>
2019-03-16 19:34:51 -0600

    mongo again

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/mon-scraper-2", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/mon-scraper-2", { useNewUrlParser: true });
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
