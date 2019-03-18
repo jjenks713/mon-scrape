f9432a976bff31eadde1a6ad190c4de5d2b541e4
Chillen <josh.jenkin@live.com>
2019-03-18 16:14:30 -0600

    users

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 if (MONGODB_URI) {
-  mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+  mongoose.connect(MONGODB_URI);
 } else {
   mongoose.connect("mongodb://localhost/mon-scraper-2", { useNewUrlParser: true });
 }
