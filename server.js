e15545010090231e2b2ec553d81a0df4eeffc3be
Chillen <josh.jenkin@live.com>
2019-03-20 17:38:14 -0600

    images

--- server.js
+++ server.js
@@ -29,8 +29,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-
-
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 // if (MONGODB_URI) {
